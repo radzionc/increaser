dd706228e08d2ccd40c200a6a6e16cf529b2fd33
Rodion Chachura <geekrodion@gmail.com>
2023-12-10 17:20:10 +0400

    wip

--- ui/charts/SplineChart/index.tsx
+++ ui/charts/SplineChart/index.tsx
@@ -10,10 +10,6 @@ interface SplineChartProps {
 }
 
 const calculateControlPoints = (dataPoints: Point[]) => {
-  // This function calculates control points for each data point.
-  // A simple implementation can just use points between the actual points,
-  // or you can implement a more advanced algorithm for smoother curves.
-  // For simplicity, this example uses midpoints as control points.
   const controlPoints = []
   for (let i = 0; i < dataPoints.length - 1; i++) {
     const current = dataPoints[i]
@@ -45,8 +41,33 @@ const createPath = (
   return path
 }
 
+const createClosedPath = (
+  points: Point[],
+  controlPoints: Point[],
+  width: number,
+  height: number,
+) => {
+  let path = `M${points[0].x * width} ${height}`
+  path += ` L${points[0].x * width} ${height - points[0].y * height}`
+
+  for (let i = 0; i < points.length - 1; i++) {
+    const current = points[i]
+    const next = points[i + 1]
+    const control = controlPoints[i]
+    path +=
+      ` C${control.x * width} ${height - current.y * height},` +
+      `${control.x * width} ${height - next.y * height},` +
+      `${next.x * width} ${height - next.y * height}`
+  }
+
+  path += ` L${points[points.length - 1].x * width} ${height}`
+  path += ' Z'
+
+  return path
+}
+
 export const SplineChart = ({ data, width, height }: SplineChartProps) => {
-  const path = useMemo(() => {
+  const [path, closedPath] = useMemo(() => {
     if (data.length === 0) return ''
 
     const normalizedData = normalize(data)
@@ -56,8 +77,10 @@ export const SplineChart = ({ data, width, height }: SplineChartProps) => {
     }))
 
     const controlPoints = calculateControlPoints(points)
-    return createPath(points, controlPoints, width, height)
-    // finish this
+    return [
+      createPath(points, controlPoints, width, height),
+      createClosedPath(points, controlPoints, width, height),
+    ]
   }, [data, height, width])
 
   const theme = useTheme()
@@ -74,6 +97,11 @@ export const SplineChart = ({ data, width, height }: SplineChartProps) => {
         fill="none"
         stroke={theme.colors.primary.toCssValue()}
         strokeWidth="2"
+      />
+      <path
+        d={closedPath}
+        fill={theme.colors.primary.getVariant({ a: () => 0.1 }).toCssValue()}
+        strokeWidth="0"
       />
     </svg>
   )
