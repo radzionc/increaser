09718ea481b6f74c38689c9cb202e351a0168179
Rodion Chachura <geekrodion@gmail.com>
2024-02-03 14:38:22 +0400

    Refactor TasksDeadlinesOverview component

--- increaser/app/tasks/components/TasksDeadlinesOverview/index.tsx
+++ increaser/app/tasks/components/TasksDeadlinesOverview/index.tsx
@@ -1,29 +1,16 @@
 import { useStartOfWeek } from '@lib/ui/hooks/useStartOfWeek'
-import { UniformColumnGrid } from '@lib/ui/layout/UniformColumnGrid'
 import { WeekDeadlinesOverview } from './WeekDeadlinesOverview'
 import { convertDuration } from '@lib/utils/time/convertDuration'
 import { splitBy } from '@lib/utils/array/splitBy'
 import { useAssertUserState } from '@increaser/ui/user/UserStateContext'
-import styled from 'styled-components'
 import { Panel } from '@lib/ui/panel/Panel'
-import { getColor } from '@lib/ui/theme/getters'
 import { ElementSizeAware } from '@lib/ui/base/ElementSizeAware'
-import { VStack } from '@lib/ui/layout/Stack'
+import styled from 'styled-components'
 
 const Container = styled(Panel)`
-  background: ${getColor('foreground')};
-`
-
-const Row = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto 1fr;
-  gap: 20px;
-`
-
-const RowSeparator = styled.div`
-  height: 100%;
-  width: 1px;
-  background: ${getColor('mistExtra')};
+  > * {
+    flex: 1;
+  }
 `
 
 export const TasksDeadlinesOverview = () => {
@@ -37,36 +24,23 @@ export const TasksDeadlinesOverview = () => {
   return (
     <ElementSizeAware
       render={({ setElement, size }) => {
-        const firstItem = (
-          <WeekDeadlinesOverview
-            name="This week"
-            tasks={thisWeekTasks}
-            startedAt={weekStartedAt}
-          />
-        )
-
-        const secondItem = (
-          <WeekDeadlinesOverview
-            name="Next week"
-            tasks={nextWeekTasks}
-            startedAt={nextWeekStartsAt}
-            showWorkdays={false}
-          />
-        )
         return (
-          <Container ref={setElement}>
-            {size && size.width < 520 ? (
-              <VStack gap={28}>
-                {firstItem}
-                {secondItem}
-              </VStack>
-            ) : (
-              <Row>
-                {firstItem}
-                <RowSeparator />
-                {secondItem}
-              </Row>
-            )}
+          <Container
+            direction={size && size.width < 520 ? 'column' : 'row'}
+            withSections
+            ref={setElement}
+          >
+            <WeekDeadlinesOverview
+              name="This week"
+              tasks={thisWeekTasks}
+              startedAt={weekStartedAt}
+            />
+            <WeekDeadlinesOverview
+              name="Next week"
+              tasks={nextWeekTasks}
+              startedAt={nextWeekStartsAt}
+              showWorkdays={false}
+            />
           </Container>
         )
       }}
