99f4e33a1d7feacf0392a923846c04d5585038dc
Rodion Chachura <geekrodion@gmail.com>
2024-02-03 14:20:42 +0400

    Refactor TasksDeadlinesOverview component

--- increaser/app/tasks/components/TasksDeadlinesOverview/WeekDeadlinesOverview.tsx
+++ increaser/app/tasks/components/TasksDeadlinesOverview/WeekDeadlinesOverview.tsx
@@ -5,15 +5,12 @@ import { convertDuration } from '@lib/utils/time/convertDuration'
 import { format } from 'date-fns'
 import { Task } from '@increaser/entities/Task'
 import { useStartOfDay } from '@lib/ui/hooks/useStartOfDay'
-import { UniformColumnGrid } from '@lib/ui/layout/UniformColumnGrid'
-import { Center } from '@lib/ui/layout/Center'
 import { getShortWeekday } from '@lib/utils/time'
-import { Panel } from '@lib/ui/panel/Panel'
 import { order } from '@lib/utils/array/order'
 import styled from 'styled-components'
 import { round } from '@lib/ui/css/round'
 import { sameDimensions } from '@lib/ui/css/sameDimensions'
-import { getColor, matchColor } from '@lib/ui/theme/getters'
+import { matchColor } from '@lib/ui/theme/getters'
 
 type WeekDeadlinesOverviewProps = {
   startedAt: number
@@ -31,11 +28,6 @@ const TaskStatus = styled.div<{ completed: boolean }>`
   })};
 `
 
-const Container = styled(Panel)`
-  height: 100%;
-  background: ${getColor('foreground')};
-`
-
 export const WeekDeadlinesOverview = ({
   startedAt,
   name,
@@ -44,44 +36,41 @@ export const WeekDeadlinesOverview = ({
   const todayStartedAt = useStartOfDay()
 
   return (
-    <Container>
-      <VStack gap={16}>
-        <VStack gap={4}>
-          <HStack alignItems="center" gap={8}>
-            <Text color="contrast" weight="semibold" size={14}>
-              {name}
-            </Text>
-            <Text weight="semibold" size={14} color="supporting">
-              {format(startedAt, 'MMM d')} -{' '}
-              {format(startedAt + convertDuration(1, 'w', 'ms'), 'MMM d')}
-            </Text>
-          </HStack>
-          <UniformColumnGrid gap={4}>
-            {range(convertDuration(1, 'w', 'd')).map((dayIndex) => {
-              const dayStartedAt =
-                startedAt + convertDuration(dayIndex, 'd', 'ms')
+    <VStack gap={16}>
+      <VStack gap={4}>
+        <HStack alignItems="center" gap={8}>
+          <Text color="contrast" weight="semibold" size={14}>
+            {name}
+          </Text>
+          <Text weight="semibold" size={14} color="supporting">
+            {format(startedAt, 'MMM d')} -{' '}
+            {format(startedAt + convertDuration(1, 'w', 'ms'), 'MMM d')}
+          </Text>
+        </HStack>
+        <HStack alignItems="center" justifyContent="space-between">
+          {range(convertDuration(1, 'w', 'd')).map((dayIndex) => {
+            const dayStartedAt =
+              startedAt + convertDuration(dayIndex, 'd', 'ms')
 
-              const isToday = todayStartedAt === dayStartedAt
-              return (
-                <Center key={dayStartedAt}>
-                  <Text
-                    size={12}
-                    weight={isToday ? 'bold' : 'regular'}
-                    color={isToday ? 'primary' : 'shy'}
-                  >
-                    {getShortWeekday(dayIndex)}
-                  </Text>
-                </Center>
-              )
-            })}
-          </UniformColumnGrid>
-        </VStack>
-        <HStack alignItems="center" gap={4} wrap="wrap">
-          {order(tasks, (task) => task.completedAt ?? 0, 'desc').map((task) => (
-            <TaskStatus completed={!!task.completedAt} key={task.id} />
-          ))}
+            const isToday = todayStartedAt === dayStartedAt
+            return (
+              <Text
+                key={dayIndex}
+                size={12}
+                weight={isToday ? 'bold' : 'regular'}
+                color={isToday ? 'primary' : 'shy'}
+              >
+                {getShortWeekday(dayIndex)}
+              </Text>
+            )
+          })}
         </HStack>
       </VStack>
-    </Container>
+      <HStack alignItems="center" gap={4} wrap="wrap">
+        {order(tasks, (task) => task.completedAt ?? 0, 'desc').map((task) => (
+          <TaskStatus completed={!!task.completedAt} key={task.id} />
+        ))}
+      </HStack>
+    </VStack>
   )
 }
--- increaser/app/tasks/components/TasksDeadlinesOverview/index.tsx
+++ increaser/app/tasks/components/TasksDeadlinesOverview/index.tsx
@@ -4,6 +4,13 @@ import { WeekDeadlinesOverview } from './WeekDeadlinesOverview'
 import { convertDuration } from '@lib/utils/time/convertDuration'
 import { splitBy } from '@lib/utils/array/splitBy'
 import { useAssertUserState } from '@increaser/ui/user/UserStateContext'
+import styled from 'styled-components'
+import { Panel } from '@lib/ui/panel/Panel'
+import { getColor } from '@lib/ui/theme/getters'
+
+const Container = styled(Panel)`
+  background: ${getColor('foreground')};
+`
 
 export const TasksDeadlinesOverview = () => {
   const weekStartedAt = useStartOfWeek()
@@ -14,18 +21,20 @@ export const TasksDeadlinesOverview = () => {
   )
 
   return (
-    <UniformColumnGrid minChildrenWidth={160} gap={20}>
-      <WeekDeadlinesOverview
-        name="This week"
-        tasks={thisWeekTasks}
-        startedAt={weekStartedAt}
-      />
-      <WeekDeadlinesOverview
-        name="Next week"
-        tasks={nextWeekTasks}
-        startedAt={nextWeekStartsAt}
-        showWorkdays={false}
-      />
-    </UniformColumnGrid>
+    <Container>
+      <UniformColumnGrid minChildrenWidth={160} gap={40}>
+        <WeekDeadlinesOverview
+          name="This week"
+          tasks={thisWeekTasks}
+          startedAt={weekStartedAt}
+        />
+        <WeekDeadlinesOverview
+          name="Next week"
+          tasks={nextWeekTasks}
+          startedAt={nextWeekStartsAt}
+          showWorkdays={false}
+        />
+      </UniformColumnGrid>
+    </Container>
   )
 }
