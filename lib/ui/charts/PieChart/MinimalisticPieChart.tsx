8f508eae98e98d22ee1ee20a728dd27f6def0c2e
Rodion Chachura <geekrodion@gmail.com>
2024-03-22 15:53:43 +0400

    Refactor pie chart component to use utility function for calculating segment angles

--- lib/ui/charts/PieChart/MinimalisticPieChart.tsx
+++ lib/ui/charts/PieChart/MinimalisticPieChart.tsx
@@ -1,77 +1,35 @@
 import { useMemo } from 'react'
-import React from 'react'
-import { useTheme } from 'styled-components'
 import { SvgArc } from './SvgArc'
-import { sum } from '@lib/utils/array/sum'
 import { ComponentWithValueProps } from '../../props'
 import { PieChartItem } from './PieChartItem'
+import { getPieChartSegmentsAngles } from './utils/getPieChartSegmentsAngles'
 
-const totalDegrees = 360
 const spaceBetweenInDegrees = 0.8
-
-interface PieChartItemWithAngle extends PieChartItem {
-  startAngle: number
-  endAngle: number
-}
-
-const getItemsWithAngles = (items: PieChartItem[]): PieChartItemWithAngle[] => {
-  const total = sum(items.map((item) => item.value))
-
-  const itemsWithAngles: PieChartItemWithAngle[] = []
-
-  items.forEach((item, index) => {
-    const startAngle = index === 0 ? 0 : itemsWithAngles[index - 1].endAngle
-    const endAngle = startAngle + (item.value / total) * totalDegrees
-
-    itemsWithAngles.push({
-      ...item,
-      startAngle,
-      endAngle,
-    })
-  })
-
-  return itemsWithAngles
-}
 
 const svgViewBoxSize = 100
 
 export const MinimalisticPieChart = ({
   value,
 }: ComponentWithValueProps<PieChartItem[]>) => {
-  const { colors } = useTheme()
-
-  const itemsWithAngles = useMemo(() => {
-    const result = getItemsWithAngles(value.filter((item) => item.value > 0))
-
-    if (!result.length) {
-      result.push({
-        value: 1,
-        color: colors.foreground,
-        startAngle: 0,
-        endAngle: totalDegrees,
-      })
-    }
-
-    return result
-  }, [colors.foreground, value])
+  const angles = useMemo(
+    () => getPieChartSegmentsAngles(value.map((item) => item.value)),
+    [value],
+  )
 
   const radius = svgViewBoxSize / 2
 
   return (
     <svg viewBox={`0 0 ${svgViewBoxSize} ${svgViewBoxSize}`}>
-      {itemsWithAngles.map(({ color, startAngle, endAngle, value }, index) => {
-        if (value === 0) {
-          return null
-        }
-
+      {value.map((value, index) => {
+        const { start, end } = angles[index]
         return (
           <SvgArc
             key={index}
-            color={color}
+            color={value.color}
             radius={radius}
             cutoutRadius={radius * 0.86}
-            startAngle={startAngle}
-            endAngle={endAngle - spaceBetweenInDegrees}
+            startAngle={start}
+            endAngle={end - spaceBetweenInDegrees}
           />
         )
       })}
