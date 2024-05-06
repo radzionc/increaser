73fb406ba31d1131cdbd8b6ca013016ebeb72e8a
Rodion Chachura <geekrodion@gmail.com>
2024-05-06 10:24:21 +0400

    Refactor CommunityPage component to add SubscribeForUpdatesPrompt and update ProductFeaturesBoard

--- increaser/app/community/components/SubscribeForUpdatesPrompt.tsx
+++ increaser/app/community/components/SubscribeForUpdatesPrompt.tsx
@@ -1,33 +1,29 @@
-import { transition } from '@lib/ui/css/transition'
+import { productXUrl, productTelegramChannelUrl } from '@increaser/config'
+import { SocialLink } from '@lib/ui/buttons/SocialLink'
 import { TelegramIcon } from '@lib/ui/icons/TelegramIcon'
+import { XIcon } from '@lib/ui/icons/XIcon'
 import { HStack } from '@lib/ui/layout/Stack'
-import { ExternalLink } from '@lib/ui/navigation/Link/ExternalLink'
 import { Panel } from '@lib/ui/panel/Panel'
 import { SectionTitle } from '@lib/ui/text/SectionTitle'
-import { getColor } from '@lib/ui/theme/getters'
-import styled from 'styled-components'
-
-const Container = styled(Panel)`
-  ${transition};
-  cursor: pointer;
-  &:hover {
-    background: ${getColor('mistExtra')};
-  }
-
-  svg {
-    font-size: 24px;
-  }
-`
+import { EmojiTextPrefix } from '../../ui/EmojiTextPrefix'
 
 export const SubscribeForUpdatesPrompt = () => {
   return (
-    <ExternalLink to="https://t.me/increaser_app">
-      <Container>
-        <HStack alignItems="center" gap={16}>
-          <TelegramIcon />
-          <SectionTitle>See Increaser Updates First on Telegram!</SectionTitle>
+    <Panel>
+      <HStack justifyContent="space-between" alignItems="center" gap={16}>
+        <SectionTitle>
+          <EmojiTextPrefix emoji="🚀" />
+          Don't miss new features
+        </SectionTitle>
+        <HStack gap={8} alignItems="center">
+          <SocialLink to={productXUrl}>
+            <XIcon />
+          </SocialLink>
+          <SocialLink to={productTelegramChannelUrl}>
+            <TelegramIcon />
+          </SocialLink>
         </HStack>
-      </Container>
-    </ExternalLink>
+      </HStack>
+    </Panel>
   )
 }
