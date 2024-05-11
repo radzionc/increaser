c6b8bc8f4a64963551aaf67ecd8ed1d58f1f2442
Rodion Chachura <geekrodion@gmail.com>
2024-05-11 15:00:54 +0400

    feat: Add new focus sound icons and update focus sound options

--- increaser/app/focus/audio/sounds/focusSoundIcon.tsx
+++ increaser/app/focus/audio/sounds/focusSoundIcon.tsx
@@ -13,6 +13,12 @@ import { WaterDropIcon } from '@lib/ui/icons/WaterDropIcon'
 import { WavesIcon } from '@lib/ui/icons/WavesIcon'
 import { WhiteNoiseIcon } from '@lib/ui/icons/WhiteNoiseIcon'
 import { WindIcon } from '@lib/ui/icons/WindIcon'
+import { BirdIcon } from '@lib/ui/icons/BirdIcon'
+import { SnowIcon } from '@lib/ui/icons/SnowIcon'
+import { KeyboardIcon } from '@lib/ui/icons/KeyboardIcon'
+import { WaterfallIcon } from '@lib/ui/icons/WaterfallIcon'
+import { RoadIcon } from '@lib/ui/icons/RoadIcon'
+import { WindowIcon } from '@lib/ui/icons/WindowIcon'
 import { ReactNode } from 'react'
 import { FocusSound } from './state/useFocusSoundsPreference'
 
@@ -32,4 +38,10 @@ export const focusSoundIcon: Record<FocusSound, ReactNode> = {
   white: <WhiteNoiseIcon />,
   pink: <PinkNoiseIcon />,
   brown: <BrownNoiseIcon />,
+  birds: <BirdIcon />,
+  snow: <SnowIcon />,
+  keyboard: <KeyboardIcon />,
+  waterfall: <WaterfallIcon />,
+  window: <WindowIcon />,
+  road: <RoadIcon />,
 }
