485b865843167000fb26c5e435de88c556538e09
Rodion Chachura <geekrodion@gmail.com>
2024-05-11 13:54:06 +0400

    Refactor FocusAudioModeSelector to use ShyOption component for improved styling and consistency

--- increaser/app/focus/audio/FocusAudioModeSelector.tsx
+++ increaser/app/focus/audio/FocusAudioModeSelector.tsx
@@ -1,38 +1,10 @@
-import { borderRadius } from '@lib/ui/css/borderRadius'
-import { centerContent } from '@lib/ui/css/centerContent'
-import { transition } from '@lib/ui/css/transition'
 import { UniformColumnGrid } from '@lib/ui/layout/UniformColumnGrid'
-import { getColor } from '@lib/ui/theme/getters'
-import styled, { css } from 'styled-components'
 import {
   focusAduioModeName,
   focusAudioModes,
   useFocusAudioMode,
 } from './state/useFocusAudioMode'
-
-const Option = styled.div<{ isSelected: boolean }>`
-  ${centerContent};
-  ${borderRadius.s};
-  padding: 8px;
-  ${transition};
-  color: ${getColor('text')};
-  font-weight: 500;
-  cursor: pointer;
-  border: 1px solid ${getColor('mist')};
-
-  ${({ isSelected }) =>
-    isSelected
-      ? css`
-          background: ${getColor('mist')};
-          color: ${getColor('contrast')};
-          border-color: transparent;
-        `
-      : css`
-          &:hover {
-            background: ${getColor('mist')};
-          }
-        `}
-`
+import { ShyOption } from '@lib/ui/buttons/ShyOption'
 
 export const FocusAudioModeSelector = () => {
   const [mode, setMode] = useFocusAudioMode()
@@ -40,13 +12,13 @@ export const FocusAudioModeSelector = () => {
   return (
     <UniformColumnGrid gap={4}>
       {focusAudioModes.map((option) => (
-        <Option
+        <ShyOption
           key={option}
           isSelected={mode === option}
           onClick={() => setMode(option)}
         >
           {focusAduioModeName[option]}
-        </Option>
+        </ShyOption>
       ))}
     </UniformColumnGrid>
   )
