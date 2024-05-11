4d7f3532d0f0a1938e2e3160fb8c14f9694f9786
Rodion Chachura <geekrodion@gmail.com>
2024-05-11 13:56:42 +0400

    Refactor FocusSoundOption to use focusSoundIcon for improved maintainability and consistency

--- increaser/app/focus/audio/sounds/FocusSoundOption.tsx
+++ increaser/app/focus/audio/sounds/FocusSoundOption.tsx
@@ -1,25 +1,9 @@
-import { ReactNode } from 'react'
 import styled from 'styled-components'
 import {
   FocusSound,
   useFocusSoundsPreference,
 } from './state/useFocusSoundsPreference'
 import { transition } from '@lib/ui/css/transition'
-import { RainIcon } from '@lib/ui/icons/RainIcon'
-import { StormIcon } from '@lib/ui/icons/StormIcon'
-import { WindIcon } from '@lib/ui/icons/WindIcon'
-import { StreamIcon } from '@lib/ui/icons/StreamIcon'
-import { WavesIcon } from '@lib/ui/icons/WavesIcon'
-import { TrainIcon } from '@lib/ui/icons/TrainIcon'
-import { FireIcon } from '@lib/ui/icons/FireIcon'
-import { MoonIcon } from '@lib/ui/icons/MoonIcon'
-import { CoffeeAltIcon } from '@lib/ui/icons/CoffeeAltIcon'
-import { ForestIcon } from '@lib/ui/icons/ForestIcon'
-import { LeavesIcon } from '@lib/ui/icons/LeavesIcon'
-import { WaterDropIcon } from '@lib/ui/icons/WaterDropIcon'
-import { WhiteNoiseIcon } from '@lib/ui/icons/WhiteNoiseIcon'
-import { PinkNoiseIcon } from '@lib/ui/icons/PinkNoiseIcon'
-import { BrownNoiseIcon } from '@lib/ui/icons/BrownNoiseIcon'
 
 import { Slider } from '@lib/ui/inputs/Slider'
 import { ComponentWithValueProps } from '@lib/ui/props'
@@ -31,6 +15,7 @@ import { UnstyledButton } from '@lib/ui/buttons/UnstyledButton'
 import { getColor, matchColor } from '@lib/ui/theme/getters'
 import { IconWrapper } from '@lib/ui/icons/IconWrapper'
 import { horizontalPadding } from '@lib/ui/css/horizontalPadding'
+import { focusSoundIcon } from './focusSoundIcon'
 
 const Container = styled(UnstyledButton)<{ isActive: boolean }>`
   width: 100%;
@@ -49,24 +34,6 @@ const SliderContainer = styled.div`
   width: 100%;
   ${horizontalPadding(8)}
 `
-
-const focusIcon: Record<FocusSound, ReactNode> = {
-  rain: <RainIcon />,
-  storm: <StormIcon />,
-  wind: <WindIcon />,
-  stream: <StreamIcon />,
-  seaside: <WavesIcon />,
-  train: <TrainIcon />,
-  fire: <FireIcon />,
-  night: <MoonIcon />,
-  coffee: <CoffeeAltIcon />,
-  forest: <ForestIcon />,
-  leaves: <LeavesIcon />,
-  water: <WaterDropIcon />,
-  white: <WhiteNoiseIcon />,
-  pink: <PinkNoiseIcon />,
-  brown: <BrownNoiseIcon />,
-}
 
 export const FocusSoundOption = ({
   value,
@@ -89,7 +56,7 @@ export const FocusSoundOption = ({
         >
           <VStack alignItems="center" gap={8}>
             <Spacer {...actionSize} />
-            <IconWrapper>{focusIcon[value]}</IconWrapper>
+            <IconWrapper>{focusSoundIcon[value]}</IconWrapper>
             <Spacer {...actionSize} />
           </VStack>
         </Container>
