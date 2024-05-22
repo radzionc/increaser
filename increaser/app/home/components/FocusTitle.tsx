2a7bda1b235dadc0a132503e7050141d455d93e1
Rodion Chachura <geekrodion@gmail.com>
2024-05-22 08:22:44 +0400

    feat: Update FocusTitle component to include RhytmicRerender and PageMetaTags
    This commit updates the FocusTitle component in the increaser/app/home/components directory. It adds the RhytmicRerender component from the lib/ui/base/RhytmicRerender module and the PageMetaTags component from the lib/next-ui/metadata/PageMetaTags module. The RhytmicRerender component is used to render the PageMetaTags component, which displays the title of the page based on the duration of the focus session.
    
    These changes improve the functionality of the FocusTitle component by dynamically updating the page title based on the duration of the focus session, providing a more informative and engaging user experience.

--- increaser/app/home/components/FocusTitle.tsx
+++ increaser/app/home/components/FocusTitle.tsx
@@ -6,30 +6,46 @@ import { FocusGoal } from '@increaser/ui/focus/FocusGoal'
 import { useProjects } from '@increaser/ui/projects/ProjectsProvider'
 import { useFocus } from '@increaser/ui/focus/FocusContext'
 import { FocusProjectSelector } from '../../focus/components/FocusProjectSelector'
+import { RhytmicRerender } from '@lib/ui/base/RhytmicRerender'
+import { PageMetaTags } from '@lib/next-ui/metadata/PageMetaTags'
+import { formatDuration } from '@lib/utils/time/formatDuration'
 
 export const FocusTitle = () => {
-  const { projectId } = useCurrentFocus()
+  const { projectId, startedAt } = useCurrentFocus()
   const { focusDuration } = useFocus()
   const { projectsRecord } = useProjects()
   return (
-    <PageTitle
-      title={
-        <HStack
-          fullWidth
-          alignItems="center"
-          justifyContent="space-between"
-          gap={8}
-          wrap="wrap"
-        >
-          <Text>
-            {projectsRecord[projectId].name} {focusDuration} min session
-          </Text>
-          <HStack wrap="wrap" alignItems="center" gap={8}>
-            <FocusProjectSelector />
-            <FocusGoal />
+    <>
+      <RhytmicRerender
+        render={() => {
+          const duration = Date.now() - startedAt
+          const title = `${formatDuration(duration, 'ms', {
+            kind: 'digitalClock',
+            minUnit: 's',
+            maxUnit: 'h',
+          })}`
+          return <PageMetaTags title={title} />
+        }}
+      />
+      <PageTitle
+        title={
+          <HStack
+            fullWidth
+            alignItems="center"
+            justifyContent="space-between"
+            gap={8}
+            wrap="wrap"
+          >
+            <Text>
+              {projectsRecord[projectId].name} {focusDuration} min session
+            </Text>
+            <HStack wrap="wrap" alignItems="center" gap={8}>
+              <FocusProjectSelector />
+              <FocusGoal />
+            </HStack>
           </HStack>
-        </HStack>
-      }
-    />
+        }
+      />
+    </>
   )
 }
