90a07a84c55c07050dcb6cae50900ae7739ac81f
Rodion Chachura <geekrodion@gmail.com>
2024-06-05 16:15:55 +0400

    feat: Add lazy loading for images in VisionImageInput component

--- increaser/ui/vision/form/VisionImageInput.tsx
+++ increaser/ui/vision/form/VisionImageInput.tsx
@@ -1,11 +1,6 @@
 import { useDropzone } from 'react-dropzone'
-import { borderRadius } from '@lib/ui/css/borderRadius'
-import { textInputHeight } from '@lib/ui/css/textInput'
-import { toSizeUnit } from '@lib/ui/css/toSizeUnit'
 import { CoverImage } from '@lib/ui/images/CoverImage'
-import { InputContainer } from '@lib/ui/inputs/InputContainer'
-import { LabelText } from '@lib/ui/inputs/LabelText'
-import { HStack } from '@lib/ui/layout/Stack'
+import { HStack, VStack } from '@lib/ui/layout/Stack'
 import { InputProps } from '@lib/ui/props'
 import { getColor } from '@lib/ui/theme/getters'
 import styled from 'styled-components'
@@ -17,21 +12,40 @@ import { useUploadFileMutation } from '../../storage/useUploadFileMutation'
 import { useEffect } from 'react'
 import { getPublicFileUrl } from '../../storage/getPublicFileUrl'
 import { SafeImage } from '@lib/ui/images/SafeImage'
+import { Text } from '@lib/ui/text'
+import { Center } from '@lib/ui/layout/Center'
+import { UnstyledButton } from '@lib/ui/buttons/UnstyledButton'
+import { TakeWholeSpace } from '@lib/ui/css/takeWholeSpace'
+import { IconWrapper } from '@lib/ui/icons/IconWrapper'
+import { interactive } from '@lib/ui/css/interactive'
+import { transition } from '@lib/ui/css/transition'
+import { TrashBinIcon } from '@lib/ui/icons/TrashBinIcon'
 
-const Container = styled.div`
+const Wrapper = styled.div`
   ${visionImageAspectRatio};
-  height: ${toSizeUnit(textInputHeight)};
-  ${borderRadius.s};
-  object-fit: cover;
-  background: ${getColor('mist')};
-  ${centerContent};
-  overflow: hidden;
-  position: relative;
+  padding: 0;
 `
 
-const LoaderContainer = styled.div`
-  position: absolute;
-  pointer-events: none;
+const RemoveButton = styled(UnstyledButton)`
+  ${centerContent};
+  font-size: 14px;
+  font-weight: 500;
+  ${transition};
+  &:hover {
+    color: ${getColor('contrast')};
+    background: ${getColor('mist')};
+  }
+`
+
+const ImageInput = styled(TakeWholeSpace)`
+  ${centerContent};
+  ${interactive};
+  ${transition};
+  font-weight: 500;
+  &:hover {
+    color: ${getColor('contrast')};
+    background: ${getColor('mist')};
+  }
 `
 
 const Image = styled(CoverImage)``
@@ -40,7 +54,7 @@ export const VisionImageInput = ({
   value,
   onChange,
 }: InputProps<string | null>) => {
-  const { mutate: uploadFile, status, data } = useUploadFileMutation()
+  const { mutate: uploadFile, status, data, reset } = useUploadFileMutation()
   useEffect(() => {
     if (data) {
       onChange(data)
@@ -65,30 +79,83 @@ export const VisionImageInput = ({
     },
   })
 
-  const imageId = value || data
-
   return (
-    <InputContainer>
-      <LabelText>Visualize your aspiration</LabelText>
-      <HStack gap={8}>
-        <div {...getRootProps()} style={{ width: '100%' }}>
-          <input {...getInputProps()} />
-          <Container>
-            {status === 'pending' ? (
-              <LoaderContainer>
-                <Spinner />
-              </LoaderContainer>
-            ) : imageId ? (
-              <SafeImage
-                render={(props) => <Image {...props} />}
-                src={getPublicFileUrl(imageId)}
-              />
-            ) : (
-              <PictureIcon />
-            )}
-          </Container>
-        </div>
-      </HStack>
-    </InputContainer>
+    <>
+      {value && (
+        <RemoveButton
+          onClick={() => {
+            reset()
+            onChange(null)
+          }}
+        >
+          <HStack alignItems="center" gap={8}>
+            <IconWrapper>
+              <TrashBinIcon />
+            </IconWrapper>
+            Remove image
+          </HStack>
+        </RemoveButton>
+      )}
+      <Wrapper>
+        {status === 'pending' ? (
+          <Center>
+            <Spinner />
+          </Center>
+        ) : value ? (
+          <SafeImage
+            render={(props) => <Image {...props} />}
+            src={getPublicFileUrl(value)}
+            fallback={<Center>Failed to load the image</Center>}
+          />
+        ) : (
+          <ImageInput {...getRootProps()}>
+            <input {...getInputProps()} />
+            <VStack alignItems="center" gap={16}>
+              <IconWrapper style={{ fontSize: 40 }}>
+                <PictureIcon />
+              </IconWrapper>
+              <VStack alignItems="center" gap={8}>
+                <Text>Add an image to visualize your aspiration</Text>
+                <Text size={14} color="supporting">
+                  Drop it here or click to select
+                </Text>
+              </VStack>
+            </VStack>
+          </ImageInput>
+        )}
+      </Wrapper>
+      {status === 'error' && (
+        <VStack>
+          <Text size={14} weight="semibold" color="alert">
+            Failed to upload the image.
+          </Text>
+        </VStack>
+      )}
+    </>
   )
+
+  // return (
+  //   <InputContainer>
+  //     <LabelText>Visualize your aspiration</LabelText>
+  //     <HStack gap={8}>
+  //       <div {...getRootProps()} style={{ width: '100%' }}>
+  //         <input {...getInputProps()} />
+  //         <Container>
+  //           {status === 'pending' ? (
+  //             <LoaderContainer>
+  //               <Spinner />
+  //             </LoaderContainer>
+  //           ) : imageId ? (
+  //             <SafeImage
+  //               render={(props) => <Image {...props} />}
+  //               src={getPublicFileUrl(imageId)}
+  //             />
+  //           ) : (
+  //             <PictureIcon />
+  //           )}
+  //         </Container>
+  //       </div>
+  //     </HStack>
+  //   </InputContainer>
+  // )
 }
