925268fff6f3ad7059339fae5492a683854d57a2
Rodion Chachura <geekrodion@gmail.com>
2024-06-06 08:09:45 +0400

    refactor: Update deletePublicBucketFile to use getPublicBucketName
    This commit refactors the deletePublicBucketFile function in the deletePublicBucketFile.ts file. It replaces the usage of the getEnvVar function to retrieve the PUBLIC_BUCKET_NAME environment variable with the getPublicBucketName function. This change improves the readability and maintainability of the code by using a more descriptive function name.
    
    Co-authored-by: Radzion <radzion@increaser.org>

--- increaser/public/deletePublicBucketFile.ts
+++ increaser/public/deletePublicBucketFile.ts
@@ -1,10 +1,10 @@
 import { DeleteObjectCommand } from '@aws-sdk/client-s3'
-import { getEnvVar } from './getEnvVar'
 import { getS3Client } from './getS3Client'
+import { getPublicBucketName } from './getPublicBucketName'
 
 export const deletePublicBucketFile = async (key: string): Promise<void> => {
   const s3Client = getS3Client()
-  const bucketName = getEnvVar('PUBLIC_BUCKET_NAME')
+  const bucketName = getPublicBucketName()
 
   const command = new DeleteObjectCommand({
     Bucket: bucketName,
