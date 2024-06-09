8135ea9ad5fd522f10dc1858293c84f35e9a4c06
Rodion Chachura <geekrodion@gmail.com>
2024-06-09 14:45:20 +0400

    Refactor ExpandableInputOpener component to use textInputHeight instead of defaultInputHeight

--- lib/ui/inputs/ExpandableInputOpener.tsx
+++ lib/ui/inputs/ExpandableInputOpener.tsx
@@ -1,6 +1,5 @@
 import styled, { css } from 'styled-components'
 import { getColor } from '../theme/getters'
-import { defaultInputHeight } from './config'
 import { UnstyledButton } from '../buttons/UnstyledButton'
 import { borderRadius } from '../css/borderRadius'
 import { centerContent } from '../css/centerContent'
@@ -8,6 +7,7 @@ import { sameDimensions } from '../css/sameDimensions'
 import { transition } from '../css/transition'
 import { ComponentWithActiveState } from '../props'
 import { getHoverVariant } from '../theme/getHoverVariant'
+import { textInputHeight } from '../css/textInput'
 
 export const ExpandableInputOpener = styled(
   UnstyledButton,
@@ -16,7 +16,7 @@ export const ExpandableInputOpener = styled(
   ${borderRadius.m}
   ${transition}
 
-  ${sameDimensions(defaultInputHeight)};
+  ${sameDimensions(textInputHeight)};
   border: 1px solid ${getColor('mist')};
 
   background: ${getColor('foreground')};
