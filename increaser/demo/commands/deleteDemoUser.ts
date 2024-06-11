a60fb8365165ba8ba0bfa5f520f7d09eab44e721
Rodion Chachura <geekrodion@gmail.com>
2024-06-11 11:40:45 +0400

    Refactor createDemoUser command and add lazy loading for images
    Refactor the createDemoUser command to improve code structure and readability. Add lazy loading for images to improve page load performance. Update the imports to use the correct modules for deleting users and copying public bucket files. Modify the logic to handle the case when the user already exists. Update the vision attributes to include image copying functionality. These changes enhance the functionality and performance of the createDemoUser command.

--- increaser/demo/commands/deleteDemoUser.ts
+++ increaser/demo/commands/deleteDemoUser.ts
@@ -1,11 +1,11 @@
-import { deleteUser, getUser } from '@increaser/db/user'
+import { getUser } from '@increaser/db/user'
 import { demoConfig } from '../config'
+import { deleteUser } from '@increaser/data-services/users/deleteUser'
 
 const deleteDemoUser = async () => {
   const user = await getUser(demoConfig.userId, ['id'])
   if (user) {
     await deleteUser(user.id)
-    console.log('User deleted')
   }
 }
 
