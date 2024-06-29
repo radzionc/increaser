b9a633e92c0e21b769cfaebd0588ff8e289c1f3c
Rodion Chachura <geekrodion@gmail.com>
2024-06-29 09:25:19 +0400

    Refactor components for better code structure and performance

--- lib/ui/theme/shared.ts
+++ lib/ui/theme/shared.ts
@@ -1,7 +1,8 @@
 import { HSLA } from '../colors/HSLA'
 
 export const sharedColors = {
-  primary: new HSLA(210, 77, 51),
+  // primary: new HSLA(210, 77, 51),
+  primary: new HSLA(206, 100, 42),
 
   white: new HSLA(0, 0, 100),
   transparent: new HSLA(0, 0, 0, 0),
