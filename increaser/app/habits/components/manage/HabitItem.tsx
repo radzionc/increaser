a4fea4d91e8bb71a411e15acca8e792a79776160
Rodion Chachura <geekrodion@gmail.com>
2024-06-30 12:14:54 +0400

    Refactor HabitItem and HabitItemContent components for improved code structure and performance

--- increaser/app/habits/components/manage/HabitItem.tsx
+++ increaser/app/habits/components/manage/HabitItem.tsx
@@ -1,14 +1,11 @@
 import styled from 'styled-components'
 import { Hoverable } from '@lib/ui/base/Hoverable'
-import { verticalPadding } from '@lib/ui/css/verticalPadding'
-import { habitVerticalPadding } from './config'
 import { useActiveItemId } from '@lib/ui/list/ActiveItemIdProvider'
 import { useCurrentHabit } from '@increaser/ui/habits/CurrentHabitProvider'
 import { EditHabitForm } from './form/EditHabitForm'
 import { HabitItemContent } from './HabitItemContent'
 
 const Container = styled(Hoverable)`
-  ${verticalPadding(habitVerticalPadding)};
   text-align: start;
   width: 100%;
 `
--- increaser/app/habits/components/manage/HabitItemContent.tsx
+++ increaser/app/habits/components/manage/HabitItemContent.tsx
@@ -1,16 +1,15 @@
 import { Text } from '@lib/ui/text'
 import styled from 'styled-components'
 import { toSizeUnit } from '@lib/ui/css/toSizeUnit'
-import { habitContentMinHeight } from './config'
 import { getColor } from '@lib/ui/theme/getters'
-import { EmojiTextPrefix } from '@lib/ui/text/EmojiTextPrefix'
 import { useCurrentHabit } from '@increaser/ui/habits/CurrentHabitProvider'
+import { ChecklistItemFrame } from '@lib/ui/checklist/ChecklistItemFrame'
+import { Center } from '@lib/ui/layout/Center'
+import { habitContentMinHeight } from './config'
 
 const Name = styled(Text)`
   text-align: start;
-  font-weight: 500;
   color: ${getColor('contrast')};
-  font-size: 14px;
   line-height: ${toSizeUnit(habitContentMinHeight)};
 `
 
@@ -18,9 +17,11 @@ export const HabitItemContent = () => {
   const { name, emoji } = useCurrentHabit()
 
   return (
-    <Name>
-      <EmojiTextPrefix emoji={emoji} />
-      {name}
-    </Name>
+    <ChecklistItemFrame>
+      <Center>
+        <Text color="contrast">{emoji}</Text>
+      </Center>
+      <Name>{name}</Name>
+    </ChecklistItemFrame>
   )
 }
