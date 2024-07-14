f238e2c7d9bb772b5357649fe7d5953ec7564bf6
Rodion Chachura <geekrodion@gmail.com>
2024-07-14 14:02:26 +0400

    Refactor GoalsReview component for improved code structure and consistency

--- increaser/app/plan/day/goals/GoalsReview.tsx
+++ increaser/app/plan/day/goals/GoalsReview.tsx
@@ -1,8 +1,14 @@
 import { VStack } from '@lib/ui/layout/Stack'
 import { ActiveGoals } from '../../../goals/ActiveGoals'
+import styled from 'styled-components'
+
+const Container = styled(VStack)`
+  max-width: 560px;
+  gap: 40px;
+`
 
 export const GoalsReview = () => (
-  <VStack gap={40}>
+  <Container>
     <ActiveGoals />
-  </VStack>
+  </Container>
 )
