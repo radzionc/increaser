dc978d9412e77650960d563f8d67df2fa860e4d5
Rodion Chachura <geekrodion@gmail.com>
2024-07-24 19:00:33 +0400

    Refactor deleteUserEntity to handle user entity removal for projects

--- increaser/api/userEntities/resolvers/deleteUserEntity.ts
+++ increaser/api/userEntities/resolvers/deleteUserEntity.ts
@@ -1,6 +1,19 @@
 import { assertUserId } from '../../auth/assertUserId'
 import * as userEntitiesDb from '@increaser/db/userEntity'
 import { ApiResolver } from '../../resolvers/ApiResolver'
+import { UserEntity } from '@increaser/entities/User'
+import { syncProjectsDependantFields } from '@increaser/data-services/projects/syncProjectsDependantFields'
+
+type UserEntityRemovalHandlerParams = {
+  userId: string
+  id: string
+}
+
+const handleUserEntityRemoval: Partial<
+  Record<UserEntity, (params: UserEntityRemovalHandlerParams) => Promise<void>>
+> = {
+  project: ({ userId }) => syncProjectsDependantFields(userId),
+}
 
 export const deleteUserEntity: ApiResolver<'deleteUserEntity'> = async ({
   input: { entity, id },
@@ -13,4 +26,7 @@ export const deleteUserEntity: ApiResolver<'deleteUserEntity'> = async ({
     entity,
     entityId: id,
   })
+
+  const handler = handleUserEntityRemoval[entity]
+  await handler?.({ userId, id })
 }
