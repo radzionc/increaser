db70587518a054e21c41d52c48360ef2c7a78b48
Rodion Chachura <geekrodion@gmail.com>
2024-07-26 15:07:10 +0400

    Refactor habits page layout for improved UI consistency and readability

--- increaser/app/pages/plan/habits.tsx
+++ increaser/app/pages/plan/habits.tsx
@@ -1,8 +1,8 @@
 import { withLayout } from '@lib/next-ui/utils/withLayout'
 import { MyHabits } from '../../habits/components/MyHabits'
-import { HabitsLayout } from '../../habits/components/HabitsLayout'
+import { PlanDayLayout } from '../../plan/day/PlanDayLayout'
 
 export default withLayout({
   page: MyHabits,
-  layout: HabitsLayout,
+  layout: PlanDayLayout,
 })
