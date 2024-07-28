31143edf594cc289f1ae2967b6004f5e3c944db3
Rodion Chachura <geekrodion@gmail.com>
2024-07-28 12:22:57 +0400

    Refactor AddTask component to use Opener and PanelModal for improved UI consistency and readability

---
+++ increaser/app/focus/launcher/AddTask.tsx
@@ -0,0 +1,59 @@
+import { Opener } from '@lib/ui/base/Opener'
+import React from 'react'
+import { FocusOptionContainer } from './FocusOptionContainer'
+import { PlusIcon } from '@lib/ui/icons/PlusIcon'
+import styled from 'styled-components'
+import { CreateTaskForm } from '@increaser/ui/tasks/form/CreateTaskForm'
+import { useFocusLauncher } from './state/FocusLauncherContext'
+import { PanelModal } from '@lib/ui/modal/PanelModal'
+import { PrefixedItemFrame } from '@lib/ui/list/PrefixedItemFrame'
+import { verticalPadding } from '@lib/ui/css/verticalPadding'
+
+const Container = styled(FocusOptionContainer)``
+
+const Content = styled(PrefixedItemFrame)`
+  ${verticalPadding(0)};
+`
+
+export const AddTask = () => {
+  const { setState } = useFocusLauncher()
+
+  return (
+    <Opener
+      renderOpener={({ onOpen, isOpen }) =>
+        isOpen ? null : (
+          <Container
+            onClick={onOpen}
+            as="button"
+            type="button"
+            selected={false}
+          >
+            <Content prefix={<PlusIcon />}>Add a task for today</Content>
+          </Container>
+        )
+      }
+      renderContent={({ onClose }) => (
+        <PanelModal width={460} onFinish={onClose}>
+          <CreateTaskForm
+            onFinish={(task) => {
+              // wait for mutation to finish
+              if (task) return
+
+              onClose()
+            }}
+            onMutationFinish={(task) => {
+              onClose()
+              if (task) {
+                setState((state) => ({
+                  ...state,
+                  projectId: task.projectId,
+                  taskId: task.id,
+                }))
+              }
+            }}
+          />
+        </PanelModal>
+      )}
+    />
+  )
+}
