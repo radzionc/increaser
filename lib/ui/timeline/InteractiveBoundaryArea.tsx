e16e697a6596da17bac5421923e2f81ab5041a89
Rodion Chachura <geekrodion@gmail.com>
2024-08-05 14:13:08 +0400

    style: Update background color in InteractiveBoundaryArea component

--- lib/ui/timeline/InteractiveBoundaryArea.tsx
+++ lib/ui/timeline/InteractiveBoundaryArea.tsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components'
-import { getColor } from '../theme/getters'
 
 export const InteractiveBoundaryArea = styled.div`
   width: 100%;
   cursor: row-resize;
   height: 10px;
-  background: ${getColor('mistExtra')};
 `
