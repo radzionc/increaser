a2805b05d0a57a537c3d6a05304d2595bc7b4f9c
Rodion Chachura <geekrodion@gmail.com>
2024-08-06 18:51:42 +0400

    Refactor Scoreboard component to improve performance and readability

---
+++ increaser/website/landing/Scoreboard.tsx
@@ -0,0 +1,44 @@
+import { QueryDependant } from '@lib/ui/query/components/QueryDependant'
+import { Spinner } from '@lib/ui/loaders/Spinner'
+import { Text } from '@lib/ui/text'
+import { Panel } from '@lib/ui/panel/Panel'
+import { HStack, VStack } from '@lib/ui/layout/Stack'
+import {
+  ScoreboardPeriod,
+  scoreboardPeriodInDays,
+} from '@increaser/entities/PerformanceScoreboard'
+import { useApiQuery } from '@increaser/api-ui/hooks/useApiQuery'
+import { SectionTitle } from '@lib/ui/text/SectionTitle'
+import { LastScoreboardUpdate } from '@increaser/ui/scoreboard/LastScoreboardUpdate'
+import { ScoreboardTable } from '@increaser/ui/scoreboard/ScoreboardTable'
+
+export const Scoreboard = () => {
+  const scoreboardPeriod: ScoreboardPeriod = 'week'
+  const query = useApiQuery('scoreboard', { id: scoreboardPeriod })
+
+  return (
+    <Panel kind="secondary">
+      <VStack gap={24}>
+        <SectionTitle>
+          Last {scoreboardPeriodInDays[scoreboardPeriod]} days top performers
+        </SectionTitle>
+        <QueryDependant
+          query={query}
+          success={(value) => (
+            <VStack gap={24}>
+              <ScoreboardTable
+                myPosition={value.myPosition}
+                users={value.users}
+              />
+              <HStack fullWidth justifyContent="end">
+                <LastScoreboardUpdate value={value.syncedAt} />
+              </HStack>
+            </VStack>
+          )}
+          error={() => <Text>Something went wrong</Text>}
+          pending={() => <Spinner />}
+        />
+      </VStack>
+    </Panel>
+  )
+}
--- increaser/website/landing/ScoreboardSlice.tsx
+++ increaser/website/landing/ScoreboardSlice.tsx
@@ -1,7 +1,7 @@
 import { WebsiteSectionHeader } from '@lib/ui/website/WebsiteSectionHeader'
 import { WebsiteSlice } from '@lib/ui/website/WebsiteSlice'
 import { WebsiteSliceContent } from '@lib/ui/website/WebsiteSliceContent'
-import { Scoreboard } from '@increaser/ui/scoreboard/Scoreboard'
+import { Scoreboard } from './Scoreboard'
 import { productName } from '@increaser/config'
 import { ApiProvider } from '../api/ApiProvider'
 
