04b986c5164990a239c2bdca904b17b530f3464f
Rodion Chachura <geekrodion@gmail.com>
2024-08-21 08:22:43 +0400

    Refactor VisionLayout component to use new UI components for better structure and readability

--- increaser/app/vision/VisionLayout.tsx
+++ increaser/app/vision/VisionLayout.tsx
@@ -1,22 +1,22 @@
 import { ComponentWithChildrenProps } from '@lib/ui/props'
 import { AppPageLayout } from '../focus/components/AppPageLayout'
-import { PageTitle } from '../ui/PageTitle'
 import { UserStateOnly } from '../user/state/UserStateOnly'
-import { FixedWidthContent } from '../components/reusable/fixed-width-content'
-import { PageViewNavigation } from '../navigation/page/PageViewNavigation'
-
-const title = 'Vision'
+import { PageContainer } from '../ui/page/PageContainer'
+import { PageContent } from '../ui/page/PageContent'
+import { PageHeader } from '../ui/page/header/PageHeader'
+import { PagePrimaryNavigation } from '../navigation/page/PagePrimaryNavigation'
 
 export const VisionLayout = ({ children }: ComponentWithChildrenProps) => {
   return (
     <AppPageLayout>
-      <FixedWidthContent>
-        <PageTitle
-          documentTitle={`⏳ ${title}`}
-          title={<PageViewNavigation />}
-        />
-        <UserStateOnly>{children}</UserStateOnly>
-      </FixedWidthContent>
+      <PageContainer>
+        <PageContent>
+          <PageHeader>
+            <PagePrimaryNavigation />
+          </PageHeader>
+          <UserStateOnly>{children}</UserStateOnly>
+        </PageContent>
+      </PageContainer>
     </AppPageLayout>
   )
 }
