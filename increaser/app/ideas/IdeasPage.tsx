0dbfa53de6fc2a279ea57c91ee88407bb66d7503
Rodion Chachura <geekrodion@gmail.com>
2024-08-21 08:20:18 +0400

    Refactor IdeasPage component to use new UI components for better structure and readability

--- increaser/app/ideas/IdeasPage.tsx
+++ increaser/app/ideas/IdeasPage.tsx
@@ -1,34 +1,31 @@
-import { PageTitle } from '@lib/ui/text/PageTitle'
-
 import { Page } from '@lib/next-ui/Page'
 import { UserStateOnly } from '../user/state/UserStateOnly'
 import { Ideas } from '@increaser/ui/ideas/Ideas'
 import { ClientOnly } from '@lib/ui/base/ClientOnly'
 import { PageContainer } from '../ui/page/PageContainer'
 import { PageContent } from '../ui/page/PageContent'
-import { PageDocumentTitle } from '../ui/page/PageDocumentTitle'
-import { Header } from '@lib/ui/layout/Header'
 
 import { ManageProjectFilter } from '@increaser/ui/projects/filter/ManageProjectFilter'
 import { ProjectFilterProvider } from '@increaser/ui/projects/filter/ProjectFilterProvider'
-
-const title = 'Ideas'
-const contentWidth = 560
+import { PageHeader } from '../ui/page/header/PageHeader'
+import { PagePrimaryNavigation } from '../navigation/page/PagePrimaryNavigation'
 
 export const IdeasPage: Page = () => {
   return (
     <PageContainer>
-      <PageContent style={{ maxWidth: contentWidth }}>
+      <PageContent>
         <ProjectFilterProvider initialValue={null}>
-          <Header>
-            <PageTitle>{title}</PageTitle>
-            <PageDocumentTitle emoji="💡" title={title} />
-            <ClientOnly>
-              <UserStateOnly>
-                <ManageProjectFilter />
-              </UserStateOnly>
-            </ClientOnly>
-          </Header>
+          <PageHeader
+            controls={
+              <ClientOnly>
+                <UserStateOnly>
+                  <ManageProjectFilter />
+                </UserStateOnly>
+              </ClientOnly>
+            }
+          >
+            <PagePrimaryNavigation />
+          </PageHeader>
           <UserStateOnly>
             <Ideas />
           </UserStateOnly>
