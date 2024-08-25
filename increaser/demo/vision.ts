a1bb2113fd33dcdad8a0cb0ea8b357813cd9edce
Rodion Chachura <geekrodion@gmail.com>
2024-08-25 11:39:32 +0400

    feat: Add description field to VisionAttributeIdea

--- increaser/demo/vision.ts
+++ increaser/demo/vision.ts
@@ -7,13 +7,14 @@ import { toRecord } from '@lib/utils/record/toRecord'
 
 export const getDemoVision = (): Vision => {
   const items: VisionAttribute[] = visionAttributeIdeas.map(
-    ({ id, name, emoji }, order) => ({
+    ({ id, name, emoji, description }, order) => ({
       id,
       name,
       status: order < 3 ? 'maintainance' : order < 5 ? 'inProgress' : 'toDo',
       imageId: `vision/${id}.webp`,
       order,
       emoji,
+      description,
     }),
   )
 
