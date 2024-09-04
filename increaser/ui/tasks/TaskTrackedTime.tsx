258be7727801d0204853e015d5eae6e7161fd13c
Rodion Chachura <geekrodion@gmail.com>
2024-09-04 09:06:03 +0400

    feat: Improve TaskTrackedTime component
    This commit adds a check in the TaskTrackedTime component to hide it if the spentTime is less than 1 minute. This check is implemented by using the convertDuration function from the @lib/utils/time/convertDuration module to convert the spentTime from milliseconds to minutes and rounding it to the nearest whole number. If the converted value is less than 1, the component is returned as null.
    
    These changes enhance the usability of the TaskTrackedTime component by preventing it from being displayed when the spentTime is negligible, improving the overall user experience.

--- increaser/ui/tasks/TaskTrackedTime.tsx
+++ increaser/ui/tasks/TaskTrackedTime.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { coloredTag } from '@lib/ui/css/coloredTag'
 import { getColor } from '@lib/ui/theme/getters'
 import { TaskTagContainer } from './TaskTagContainer'
+import { convertDuration } from '@lib/utils/time/convertDuration'
 
 const Container = styled(TaskTagContainer)`
   ${({ theme: { colors } }) => coloredTag(colors.primary)};
@@ -14,7 +15,9 @@ const Container = styled(TaskTagContainer)`
 export const TaskTrackedTime = () => {
   const { spentTime } = useCurrentTask()
 
-  if (!spentTime) return null
+  if (!spentTime || Math.round(convertDuration(spentTime, 'ms', 'min')) < 1) {
+    return null
+  }
 
   return <Container>{formatDuration(spentTime, 'ms')}</Container>
 }
