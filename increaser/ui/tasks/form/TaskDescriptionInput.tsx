423b3549f3ad00809a5300257d747633c1a0c835
Rodion Chachura <geekrodion@gmail.com>
2024-09-08 10:51:21 +0400

    ```text feat: Update TaskDescriptionInput to use MultilineTextInput
    This commit updates the TaskDescriptionInput component to use the MultilineTextInput component from the '@lib/ui/inputs' library instead of the EmbeddedDescriptionInput component. The MultilineTextInput component provides a more flexible and customizable input field for the task description, improving the user experience. Additionally, the background color of the input field is set to the 'background' color from the theme using the getColor function, ensuring consistency with the rest of the application's styling.

--- increaser/ui/tasks/form/TaskDescriptionInput.tsx
+++ increaser/ui/tasks/form/TaskDescriptionInput.tsx
@@ -1,14 +1,20 @@
-import { EmbeddedDescriptionInput } from '@lib/ui/inputs/EmbeddedDescriptionInput'
+import { MultilineTextInput } from '@lib/ui/inputs/MultilineTextInput'
 import { InputProps } from '@lib/ui/props'
+import { getColor } from '@lib/ui/theme/getters'
+import styled from 'styled-components'
+
+const Container = styled(MultilineTextInput)`
+  background: ${getColor('background')};
+  /* margin-top: -2px; */
+`
 
 export const TaskDescriptionInput = ({
   value,
   onChange,
 }: InputProps<string>) => {
   return (
-    <EmbeddedDescriptionInput
-      label="Description"
-      placeholder="Describe your task"
+    <Container
+      placeholder="Add description..."
       onChange={onChange}
       value={value}
     />
