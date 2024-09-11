839e0a0c6c5c160e1cc42c9ebb0306bced243214
Rodion Chachura <geekrodion@gmail.com>
2024-09-11 08:53:29 +0400

    Refactor task checklist item input to improve manageability and reduce space usage

--- increaser/ui/tasks/form/checklist/ChecklistRemoveButton.tsx
+++ increaser/ui/tasks/form/checklist/ChecklistRemoveButton.tsx
@@ -1,20 +1,10 @@
 import { IconButton } from '@lib/ui/buttons/IconButton'
-import { borderRadius } from '@lib/ui/css/borderRadius'
-import { sameDimensions } from '@lib/ui/css/sameDimensions'
 import { TrashBinIcon } from '@lib/ui/icons/TrashBinIcon'
-import { tightListItemMinHeight } from '@lib/ui/list/tightListItemConfig'
 import { ClickableComponentProps } from '@lib/ui/props'
-import styled from 'styled-components'
-
-const Container = styled(IconButton)`
-  ${sameDimensions(tightListItemMinHeight)};
-  ${borderRadius.xs};
-`
 
 export const ChecklistRemoveButton = ({ onClick }: ClickableComponentProps) => (
-  <Container
-    kind="alertSecondary"
-    size="s"
+  <IconButton
+    kind="alert"
     onClick={onClick}
     icon={<TrashBinIcon />}
     title="Remove item"
--- increaser/ui/tasks/form/checklist/TaskChecklistItemInput.tsx
+++ increaser/ui/tasks/form/checklist/TaskChecklistItemInput.tsx
@@ -7,11 +7,31 @@ import { ChecklistRemoveButton } from './ChecklistRemoveButton'
 import { ChecklistItemNameInput } from './ChecklistItemNameInput'
 import styled from 'styled-components'
 import { CursorPosition } from '@lib/ui/entities/CursorPosition'
+import { centerContent } from '@lib/ui/css/centerContent'
+import { toSizeUnit } from '@lib/ui/css/toSizeUnit'
+import { tightListItemMinHeight } from '@lib/ui/list/tightListItemConfig'
 
 type TaskChecklistItemInputProps = InputProps<TaskChecklistItem> & {
   onRemove: () => void
   onSubmit?: (position: CursorPosition) => void
 }
+
+const ButtonWrapper = styled.div`
+  height: ${toSizeUnit(tightListItemMinHeight)};
+  ${centerContent}
+
+  @media (hover: hover) and (pointer: fine) {
+    opacity: 0;
+  }
+`
+
+const Wrapper = styled(HStack)`
+  width: 100%;
+
+  &:hover ${ButtonWrapper}, &:focus-within ${ButtonWrapper} {
+    opacity: 1;
+  }
+`
 
 const Container = styled(ChecklistItemFrame)`
   align-items: start;
@@ -25,7 +45,7 @@ export const TaskChecklistItemInput = ({
   onSubmit,
 }: TaskChecklistItemInputProps) => {
   return (
-    <HStack fullWidth>
+    <Wrapper>
       <Container>
         <ChecklistCheckbox
           value={value.completed}
@@ -38,7 +58,9 @@ export const TaskChecklistItemInput = ({
           onRemove={onRemove}
         />
       </Container>
-      <ChecklistRemoveButton onClick={onRemove} />
-    </HStack>
+      <ButtonWrapper>
+        <ChecklistRemoveButton onClick={onRemove} />
+      </ButtonWrapper>
+    </Wrapper>
   )
 }
