cbf3e4e027112a992d8b7e76ea21ccb700dd7d7d
Rodion Chachura <geekrodion@gmail.com>
2024-09-15 18:24:36 +0400

    Refactor ActiveFocusTime component to improve code organization and functionality

--- increaser/app/focus/components/FocusSetWidget/ActiveFocusTime.tsx
+++ increaser/app/focus/components/FocusSetWidget/ActiveFocusTime.tsx
@@ -6,40 +6,52 @@ import {
 import { TitleFocusDurationSelector } from '../../../home/components/TitleFocusDurationSelector'
 import { SessionIntervals } from './SessionIntervals'
 import { Text } from '@lib/ui/text'
-import { round } from '@lib/ui/css/round'
 import { getColor } from '@lib/ui/theme/getters'
 import styled from 'styled-components'
+import { borderRadius } from '@lib/ui/css/borderRadius'
+import { TakeWholeSpaceAbsolutely } from '@lib/ui/css/takeWholeSpaceAbsolutely'
+import { horizontalPadding } from '@lib/ui/css/horizontalPadding'
+import { centerContent } from '@lib/ui/css/centerContent'
 
-const FillerContainer = styled.div`
-  height: 2px;
-  width: 100%;
-  bottom: -8px;
-  position: absolute;
-  ${round};
+const Container = styled.div`
+  ${borderRadius.s};
+  padding: 2px;
   background: ${getColor('mistExtra')};
+  height: 40px;
   overflow: hidden;
+  position: relative;
+`
+
+const Content = styled.div`
+  background: ${getColor('background')};
+  height: 100%;
+  border-radius: 6px;
+  ${horizontalPadding(12)};
+  ${centerContent};
 `
 
 export const ActiveFocusTime = () => {
   return (
-    <div style={{ position: 'relative' }}>
-      <HStackSeparatedBy
-        style={{ position: 'relative' }}
-        separator={
-          <Text color="shy" as="span">
-            {slashSeparator}
-          </Text>
-        }
-        gap={12}
-      >
-        <Text as="div" weight="600" size={32} height="small">
-          <FocusPassedTime />
-        </Text>
-        <TitleFocusDurationSelector />
-      </HStackSeparatedBy>
-      <FillerContainer>
+    <Container>
+      <TakeWholeSpaceAbsolutely style={{ zIndex: -1 }}>
         <SessionIntervals />
-      </FillerContainer>
-    </div>
+      </TakeWholeSpaceAbsolutely>
+      <Content>
+        <HStackSeparatedBy
+          style={{ position: 'relative' }}
+          separator={
+            <Text color="shy" as="span">
+              {slashSeparator}
+            </Text>
+          }
+          gap={12}
+        >
+          <Text as="div" weight="600" size={24} height="small">
+            <FocusPassedTime />
+          </Text>
+          <TitleFocusDurationSelector />
+        </HStackSeparatedBy>
+      </Content>
+    </Container>
   )
 }
