417f17009db2f038a22c4bc83cd7c37966f82965
Rodion Chachura <geekrodion@gmail.com>
2024-09-18 09:36:46 +0400

    Refactor WorkdayLeftBlock component to use the assert user state hook and calculate the workday end time dynamically

--- increaser/ui/sets/manager/overview/WorkdayLeftBlock.tsx
+++ increaser/ui/sets/manager/overview/WorkdayLeftBlock.tsx
@@ -5,7 +5,7 @@ import { toPercents } from '@lib/utils/toPercents'
 import { convertDuration } from '@lib/utils/time/convertDuration'
 import { useSelectedWeekday } from '@lib/ui/time/SelectedWeekdayProvider'
 import { useStartOfWeekday } from '@lib/ui/time/hooks/useStartOfWeekday'
-import { useWorkDayEndsAt } from '../../../schedule/hooks/useWorkDayEndsAt'
+import { useAssertUserState } from '../../../user/UserStateContext'
 
 const Container = styled.div`
   width: 100%;
@@ -20,7 +20,10 @@ export const WorkdayLeftBlock = () => {
   const { startHour, endHour, currentTime } = useDayOverview()
   const timelineStartsAt = dayStartedAt + convertDuration(startHour, 'h', 'ms')
   const timelineEndsAt = dayStartedAt + convertDuration(endHour, 'h', 'ms')
-  const workdayEndsAt = useWorkDayEndsAt()
+  const { finishWorkAt } = useAssertUserState()
+
+  const workdayEndsAt =
+    dayStartedAt + convertDuration(finishWorkAt, 'min', 'ms')
 
   const workEndsIn = workdayEndsAt - currentTime
   const timespan = timelineEndsAt - timelineStartsAt
