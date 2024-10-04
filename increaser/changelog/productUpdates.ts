3b2a112e3f81032fa86c8ad4d68f318899edb4f2
Rodion Chachura <geekrodion@gmail.com>
2024-10-04 15:06:15 +0400

    Refactor import path in UserTracker.tsx to fix error in setUserIdForErrorMonitoring import Refactor import paths in UserManager.tsx and index.tsx Refactor eslint rules to include react/jsx-key error Refactor trimTextWithoutCuttingWords to use alphanumeric characters only for truncation Refactor AddYouTubeVideoOverlay component to update user preferences and improve user experience Add initial product update entry in productUpdates.ts Create latest.md file for YouTube changelog

--- increaser/changelog/productUpdates.ts
+++ increaser/changelog/productUpdates.ts
@@ -2,6 +2,19 @@ import { ProductUpdate } from './ProductUpdate'
 
 export const productUpdates: ProductUpdate[] = [
   {
+    releasedAt: 0,
+    name: '',
+    description: '',
+    items: [],
+    youtube: '',
+    telegram: '',
+    x: '',
+    linkedIn: '',
+    indieHackers: '',
+    reddit: '',
+  },
+
+  {
     releasedAt: 1727537960954,
     name: 'More Control Over Reports and Filters',
     description:
