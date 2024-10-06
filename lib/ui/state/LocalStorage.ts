d918715b69541c4150278f7263077fa1fcbd83ee
Rodion Chachura <geekrodion@gmail.com>
2024-10-06 13:58:58 +0400

    improve: local storage

--- lib/ui/state/LocalStorage.ts
+++ lib/ui/state/LocalStorage.ts
@@ -1,39 +1,73 @@
 import { OnValueChangeListener, PersistentStorage } from './PersistentStorage'
+import { recordMap } from '@lib/utils/record/recordMap'
+import { ValueTransition } from '@lib/utils/entities/ValueTransition'
 
 export class LocalStorage<T extends string> implements PersistentStorage<T> {
   listeners: Record<string, OnValueChangeListener<any>[]> = {}
 
-  getItem<V>(key: T) {
-    const item = localStorage.getItem(key)
-
-    if (item === null) return undefined
-
-    if (item === 'null') return null as never as V
-    if (item === 'undefined') return undefined
-
-    try {
-      return JSON.parse(item) as V
-    } catch {
-      return item as never as V
-    }
+  constructor() {
+    window.addEventListener('storage', this.handleStorageEvent)
   }
-  setItem<V>(key: T, value: V) {
-    const oldValue = this.getItem(key)
-    const newValue = JSON.stringify(value)
-    if (oldValue === newValue) return
 
-    if (value === undefined) {
-      localStorage.removeItem(key)
-    } else {
-      localStorage.setItem(key, newValue)
-    }
+  private handleStorageEvent = (event: StorageEvent) => {
+    const { key, newValue, oldValue } = event
 
+    if (!key) return
+
+    this.notifyListeners(
+      key,
+      recordMap({ oldValue, newValue }, this.parseValue),
+    )
+  }
+
+  private notifyListeners = <V>(
+    key: string,
+    transition: ValueTransition<V>,
+  ): void => {
     const listeners = this.listeners[key] || []
 
     listeners.forEach((listener) => {
-      listener(value, oldValue)
+      listener(transition)
     })
   }
+
+  private parseValue = <V>(value: string | null): V | undefined => {
+    if (value === null) return undefined
+    if (value === 'null') return null as unknown as V
+    if (value === 'undefined') return undefined
+
+    try {
+      return JSON.parse(value) as V
+    } catch {
+      return value as unknown as V
+    }
+  }
+
+  getItem<V>(key: T): V | undefined {
+    const item = localStorage.getItem(key)
+
+    return this.parseValue<V>(item)
+  }
+
+  setItem<V>(key: T, newValue: V): void {
+    const oldValue = this.getItem<V>(key)
+    const newValueString = JSON.stringify(newValue)
+    const oldValueString = JSON.stringify(oldValue)
+
+    if (oldValueString === newValueString) return
+
+    if (newValue === undefined) {
+      localStorage.removeItem(key)
+    } else {
+      localStorage.setItem(key, newValueString)
+    }
+
+    this.notifyListeners(key, {
+      oldValue,
+      newValue,
+    })
+  }
+
   addValueChangeListener<V>(
     key: string,
     listener: OnValueChangeListener<V>,
@@ -44,9 +78,10 @@ export class LocalStorage<T extends string> implements PersistentStorage<T> {
 
     this.listeners[key].push(listener)
   }
-  removeValueChangeListener<T>(
+
+  removeValueChangeListener<V>(
     key: string,
-    listener: OnValueChangeListener<T>,
+    listener: OnValueChangeListener<V>,
   ): void {
     this.listeners[key] = (this.listeners[key] || []).filter(
       (l) => l !== listener,
