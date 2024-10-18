a5b51379301d4becdfe0922b0cbacca32287de3a
Rodion Chachura <geekrodion@gmail.com>
2024-10-18 13:56:35 +0400

    Refactor ResetHabitsOverlay and ResetHabitsPrompt components

--- increaser/app/habits/components/track/reset/ResetHabitsPrompt.tsx
+++ increaser/app/habits/components/track/reset/ResetHabitsPrompt.tsx
@@ -1,24 +1,49 @@
-import { Opener } from '@lib/ui/base/Opener'
 import { Button } from '@lib/ui/buttons/Button'
-import { ResetHabitsOverlay } from './ResetHabitsOverlay'
 import { HStack } from '@lib/ui/css/stack'
 import { IconWrapper } from '@lib/ui/icons/IconWrapper'
 import { RefreshIcon } from '@lib/ui/icons/RefreshIcon'
+import { useHabits } from '@increaser/ui/habits/HabitsContext'
+import { useStartOfDay } from '@lib/ui/hooks/useStartOfDay'
+import { convertDuration } from '@lib/utils/time/convertDuration'
+import { useUpdateUserEntitiesMutation } from '@increaser/ui/userEntity/api/useUpdateUserEntitiesMutation'
 
 export const ResetHabitsPrompt = () => {
+  const { habits } = useHabits()
+
+  const todayStartedAt = useStartOfDay()
+
+  const areAllHabitsStartedToday = habits.every(
+    ({ startedAt }) => convertDuration(startedAt, 's', 'ms') >= todayStartedAt,
+  )
+
+  const { mutate: updateHabits } = useUpdateUserEntitiesMutation('habit')
+
+  if (areAllHabitsStartedToday) {
+    return null
+  }
+
   return (
-    <Opener
-      renderOpener={({ onOpen }) => (
-        <Button kind="secondary" size="s" onClick={onOpen}>
-          <HStack alignItems="center" gap={8}>
-            <IconWrapper>
-              <RefreshIcon />
-            </IconWrapper>
-            Reset habits
-          </HStack>
-        </Button>
-      )}
-      renderContent={({ onClose }) => <ResetHabitsOverlay onFinish={onClose} />}
-    />
+    <Button
+      kind="secondary"
+      size="s"
+      onClick={() => {
+        updateHabits(
+          habits.map(({ id }) => ({
+            id,
+            fields: {
+              startedAt: convertDuration(Date.now(), 'ms', 's'),
+              successes: [],
+            },
+          })),
+        )
+      }}
+    >
+      <HStack alignItems="center" gap={8}>
+        <IconWrapper>
+          <RefreshIcon />
+        </IconWrapper>
+        Reset habits
+      </HStack>
+    </Button>
   )
 }
