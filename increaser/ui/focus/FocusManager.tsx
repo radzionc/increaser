653dd44c85fb5ac77a08a06fb32c49db8653ba69
Rodion Chachura <geekrodion@gmail.com>
2024-10-29 10:50:48 +0400

    Refactor FocusManager to conditionally render components based on focus status

--- increaser/ui/focus/FocusManager.tsx
+++ increaser/ui/focus/FocusManager.tsx
@@ -1,33 +1,39 @@
 import { FocusSoundsPlayer } from '@increaser/app/focus/audio/sounds/FocusSoundsPlayer'
 import { YouTubeFocusMusicFloatingPlayer } from '@increaser/app/focus/audio/youTube/YouTubeFocusMusicFloatingPlayer'
-import { ActiveFocusOnly } from '@increaser/app/focus/components/ActiveFocusOnly'
-import { FocusOnly } from '@increaser/app/focus/components/FocusOnly'
 import { FocusTaskObserver } from '@increaser/app/focus/components/FocusTaskObserver'
-import { PausedFocusOnly } from '@increaser/app/focus/components/PausedFocusOnly'
 import { FocusNotifications } from '@increaser/app/focus/notifications/FocusNotifications'
 import { PausedFocusAutoStop } from './PausedFocusAutoStop'
 import { FocusAutoStop } from './FocusAutoStop'
 import { LastSetObserver } from './autoStop/LastSetObserver'
 import { FocusDurationManager } from './FocusDurationManager'
-import { NoFocusOnly } from '@increaser/app/focus/components/NoFocusOnly'
+import { useFocusStatus } from '@increaser/app/focus/state/focusIntervals'
+import { Match } from '@lib/ui/base/Match'
 
-export const FocusManager = () => (
-  <>
-    <ActiveFocusOnly>
-      <FocusSoundsPlayer />
-      <YouTubeFocusMusicFloatingPlayer />
-      <FocusNotifications />
-      <FocusAutoStop />
-    </ActiveFocusOnly>
-    <FocusOnly>
-      <FocusTaskObserver />
-    </FocusOnly>
-    <PausedFocusOnly>
-      <PausedFocusAutoStop />
-    </PausedFocusOnly>
-    <LastSetObserver />
-    <NoFocusOnly>
-      <FocusDurationManager />
-    </NoFocusOnly>
-  </>
-)
+export const FocusManager = () => {
+  const status = useFocusStatus()
+
+  return (
+    <>
+      <LastSetObserver />
+      {status === null ? (
+        <FocusDurationManager />
+      ) : (
+        <>
+          <FocusTaskObserver />
+          <Match
+            value={status}
+            active={() => (
+              <>
+                <FocusSoundsPlayer />
+                <YouTubeFocusMusicFloatingPlayer />
+                <FocusNotifications />
+                <FocusAutoStop />
+              </>
+            )}
+            paused={() => <PausedFocusAutoStop />}
+          />
+        </>
+      )}
+    </>
+  )
+}
