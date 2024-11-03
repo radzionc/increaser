bc5c391288c114f71ea1009c052cd25a1340961c
Rodion Chachura <geekrodion@gmail.com>
2024-11-03 10:36:33 +0400

    Enhance TaskFactoryItemContent to display task cadence and deadline index; improve task representation with dynamic text formatting

--- increaser/ui/taskFactories/TaskFactoryItemContent.tsx
+++ increaser/ui/taskFactories/TaskFactoryItemContent.tsx
@@ -1,12 +1,14 @@
 import { HStack } from '@lib/ui/css/stack'
 import { useCurrentTaskFactory } from './CurrentTaskFactoryProvider'
-import { TaskCadence } from './TaskCadence'
 import { PrefixedItemFrame } from '@lib/ui/list/PrefixedItemFrame'
 import { Text } from '@lib/ui/text'
 import { ProjectEmoji } from '../projects/ProjectEmoji'
+import { Match } from '@lib/ui/base/Match'
+import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
+import { WEEKDAYS } from '@lib/utils/time'
 
 export const TaskFactoryItemContent = () => {
-  const { name, projectId } = useCurrentTaskFactory()
+  const { name, projectId, cadence, deadlineIndex } = useCurrentTaskFactory()
 
   return (
     <PrefixedItemFrame
@@ -17,8 +19,22 @@ export const TaskFactoryItemContent = () => {
       }
     >
       <HStack fullWidth gap={20}>
-        <Text style={{ flex: 1 }}>{name}</Text>
-        <TaskCadence />
+        <Text style={{ flex: 1 }}>
+          {name}{' '}
+          <Text as="span" color="supporting">
+            (
+            <Match
+              value={cadence}
+              day={() => 'daily'}
+              workday={() => 'every workday'}
+              week={() =>
+                `weekly on ${WEEKDAYS[shouldBePresent(deadlineIndex)]}`
+              }
+              month={() => `monthly on the ${deadlineIndex}th`}
+            />
+            )
+          </Text>
+        </Text>
       </HStack>
     </PrefixedItemFrame>
   )
