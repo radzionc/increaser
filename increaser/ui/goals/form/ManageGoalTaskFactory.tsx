06ec4f2a150b133f9d42822f20aa2631e0ba1e8f
Rodion Chachura <geekrodion@gmail.com>
2024-11-03 15:01:38 +0400

    Enhance ManageGoalTaskFactory; replace TaskFactoryItemContent with PrefixedItemFrame and TaskFactoryTitle for improved UI and clarity, and add cadence display functionality.

--- increaser/ui/goals/form/ManageGoalTaskFactory.tsx
+++ increaser/ui/goals/form/ManageGoalTaskFactory.tsx
@@ -2,14 +2,31 @@ import { RemovableComponentProps } from '@lib/ui/props'
 import { useActiveItemId } from '@lib/ui/list/ActiveItemIdProvider'
 import { useCurrentTaskFactory } from '../../taskFactories/CurrentTaskFactoryProvider'
 import { useDeleteUserEntityMutation } from '../../userEntity/api/useDeleteUserEntityMutation'
-import { TaskFactoryItemContent } from '../../taskFactories/TaskFactoryItemContent'
 import { GoalLinkedEntity } from './linkedEntity/GoalLinkedEntity'
+import { TaskFactoryTitle } from '../../taskFactories/TaskFactoryTitle'
+import { PrefixedItemFrame } from '@lib/ui/list/PrefixedItemFrame'
+import styled from 'styled-components'
+import { takeWholeSpace } from '@lib/ui/css/takeWholeSpace'
+import { borderRadius } from '@lib/ui/css/borderRadius'
+import { centerContent } from '@lib/ui/css/centerContent'
+import { match } from '@lib/utils/match'
+import { getColor } from '@lib/ui/theme/getters'
+
+const Prefix = styled.div`
+  ${takeWholeSpace};
+  ${borderRadius.xs};
+  background: ${getColor('foreground')};
+  color: ${getColor('textPrimary')};
+  text-transform: uppercase;
+  ${centerContent};
+  font-size: 12px;
+`
 
 export const ManageGoalTaskFactory = ({
   onRemove,
 }: RemovableComponentProps) => {
   const [, setActiveItemId] = useActiveItemId()
-  const { id } = useCurrentTaskFactory()
+  const { id, cadence } = useCurrentTaskFactory()
 
   const { mutate: deleteTaskFactory } =
     useDeleteUserEntityMutation('taskFactory')
@@ -24,7 +41,21 @@ export const ManageGoalTaskFactory = ({
       }}
       onUnlink={onRemove}
     >
-      <TaskFactoryItemContent />
+      <PrefixedItemFrame
+        prefixWidth={24}
+        prefix={
+          <Prefix>
+            {match(cadence, {
+              day: () => 'd',
+              workday: () => 'wd',
+              week: () => 'w',
+              month: () => 'm',
+            })}
+          </Prefix>
+        }
+      >
+        <TaskFactoryTitle />
+      </PrefixedItemFrame>
     </GoalLinkedEntity>
   )
 }
