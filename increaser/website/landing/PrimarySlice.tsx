93386837c3c51e05361efcf0be2ee150d60b0a3e
Rodion Chachura <geekrodion@gmail.com>
2024-11-05 09:17:07 +0400

    Refactor PrimarySlice component for improved layout and add prompt for sunrise illustration

--- increaser/website/landing/PrimarySlice.tsx
+++ increaser/website/landing/PrimarySlice.tsx
@@ -7,12 +7,7 @@ import { SparkleIcon } from '@lib/ui/icons/SparkleIcon'
 import styled from 'styled-components'
 import { getColor } from '@lib/ui/theme/getters'
 
-const primaryTools = [
-  'Deep Work',
-  'Time Tracking',
-  'Daily Habits',
-  'Tasks Management',
-]
+const primaryTools = ['Deep Work', 'Time Tracking', 'Tasks', 'Daily Habits']
 
 const Separator = styled(SparkleIcon)`
   color: ${getColor('contrast')};
@@ -23,12 +18,31 @@ export const PrimarySlice = () => (
   <PrimaryWebsiteSlice>
     <VStack flexGrow justifyContent="center" alignItems="center" gap={32}>
       <VStack alignItems="center" gap={12}>
-        <Text color="contrast" size={40} as="h1" weight={800}>
+        <Text
+          centerHorizontally
+          color="contrast"
+          size={40}
+          as="h1"
+          weight={800}
+        >
           Achieve Your Goals
         </Text>
-        <HStackSeparatedBy gap={8} separator={<Separator />}>
+        <HStackSeparatedBy
+          alignItems="center"
+          justifyContent="center"
+          wrap="wrap"
+          gap={8}
+          separator={<Separator />}
+        >
           {primaryTools.map((tool) => (
-            <Text as="h2" color="primary" size={14} weight={600} key={tool}>
+            <Text
+              nowrap
+              as="h2"
+              color="primary"
+              size={14}
+              weight={600}
+              key={tool}
+            >
               {tool}
             </Text>
           ))}
