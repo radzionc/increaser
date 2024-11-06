69a06e61b299e522f15ebab3f98c8ad45ef1725e
Rodion Chachura <geekrodion@gmail.com>
2024-11-06 08:03:36 +0400

    Refactor ClosingArgumentSlice and PrimarySlice components for improved structure and styling; add scrollable prop to stack utility

--- increaser/website/landing/ClosingArgumentSlice.tsx
+++ increaser/website/landing/ClosingArgumentSlice.tsx
@@ -1,19 +1,15 @@
-import { darkTheme } from '@lib/ui/theme/darkTheme'
 import { WebsiteSectionHeader } from '@lib/ui/website/WebsiteSectionHeader'
 import { WebsiteSliceContent } from '@lib/ui/website/WebsiteSliceContent'
-import { ThemeProvider } from 'styled-components'
 import { PrimaryCallToAction } from './PrimaryCallToAction'
 import { ClosingArgumentBackground } from '@lib/ui/website/ClosingArgumentBackground'
 
 export const ClosingArgumentSlice = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <ClosingArgumentBackground imageUrl="images/closing-argument.webp">
-        <WebsiteSliceContent style={{ gap: 28, paddingBottom: 200 }}>
-          <WebsiteSectionHeader title="Unleash Your Potential" />
-          <PrimaryCallToAction />
-        </WebsiteSliceContent>
-      </ClosingArgumentBackground>
-    </ThemeProvider>
+    <ClosingArgumentBackground imageUrl="images/closing-argument.webp">
+      <WebsiteSliceContent style={{ gap: 28, paddingBottom: 200 }}>
+        <WebsiteSectionHeader title="Unleash Your Potential" />
+        <PrimaryCallToAction />
+      </WebsiteSliceContent>
+    </ClosingArgumentBackground>
   )
 }
--- increaser/website/landing/PrimarySlice.tsx
+++ increaser/website/landing/PrimarySlice.tsx
@@ -1,11 +1,13 @@
 import { PrimaryWebsiteSlice } from '@lib/ui/website/WebsiteSlice'
 import { PrimaryCallToAction } from './PrimaryCallToAction'
-import { VStack } from '@lib/ui/css/stack'
+import { vStack, VStack } from '@lib/ui/css/stack'
 import { Text } from '@lib/ui/text'
 import { HStackSeparatedBy } from '@lib/ui/layout/StackSeparatedBy'
 import { SparkleIcon } from '@lib/ui/icons/SparkleIcon'
 import styled from 'styled-components'
 import { getColor } from '@lib/ui/theme/getters'
+import { ClosingArgumentBackground } from '@lib/ui/website/ClosingArgumentBackground'
+import { borderRadius } from '@lib/ui/css/borderRadius'
 
 const primaryTools = ['Deep Work', 'Time Tracking', 'Tasks', 'Daily Habits']
 
@@ -14,9 +16,28 @@ const Separator = styled(SparkleIcon)`
   font-size: 14px;
 `
 
+const Container = styled(ClosingArgumentBackground)`
+  padding-top: 100px;
+  align-items: start;
+`
+
+const Content = styled.div`
+  z-index: 1;
+  ${vStack({
+    alignItems: 'center',
+    gap: 32,
+  })}
+  padding: 20px;
+  border-radius: 20px;
+
+  position: relative;
+
+  background: hsl(220deg 25.71% 13.73%);
+`
+
 export const PrimarySlice = () => (
-  <PrimaryWebsiteSlice>
-    <VStack flexGrow justifyContent="center" alignItems="center" gap={32}>
+  <Container imageUrl="images/mountain.jpg">
+    <Content>
       <VStack alignItems="center" gap={12}>
         <Text
           centerHorizontally
@@ -24,31 +45,45 @@ export const PrimarySlice = () => (
           size={40}
           as="h1"
           weight={800}
+          height="s"
         >
           Achieve Your Goals
         </Text>
-        <HStackSeparatedBy
-          alignItems="center"
-          justifyContent="center"
-          wrap="wrap"
-          gap={8}
-          separator={<Separator />}
+        <Text
+          color="contrast"
+          size={14}
+          weight={600}
+          style={{ fontStyle: 'italic' }}
         >
-          {primaryTools.map((tool) => (
-            <Text
-              nowrap
-              as="h2"
-              color="primary"
-              size={14}
-              weight={600}
-              key={tool}
-            >
-              {tool}
-            </Text>
-          ))}
-        </HStackSeparatedBy>
+          with
+        </Text>
+        <VStack alignItems="center" gap={8}>
+          <HStackSeparatedBy
+            alignItems="center"
+            justifyContent="center"
+            wrap="wrap"
+            gap={8}
+            separator={<Separator />}
+          >
+            {primaryTools.map((tool) => (
+              <Text
+                nowrap
+                as="h2"
+                size={16}
+                weight={700}
+                key={tool}
+                color="primary"
+              >
+                {tool}
+              </Text>
+            ))}
+          </HStackSeparatedBy>
+          <Text color="primary" nowrap as="h2" size={14} weight={600}>
+            and other cool tools...
+          </Text>
+        </VStack>
       </VStack>
       <PrimaryCallToAction />
-    </VStack>
-  </PrimaryWebsiteSlice>
+    </Content>
+  </Container>
 )
