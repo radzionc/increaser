43b2f50519354e7038587eaebab2684afbfcb2ae
Rodion Chachura <geekrodion@gmail.com>
2024-11-06 15:06:44 +0400

    Refactor useSelectedInterval to utilize dynamic persistent state keys based on time grouping; update PersistentStateKey for improved interval management

--- increaser/ui/projects/trackedTimeReport/interval/useSelectedInterval.ts
+++ increaser/ui/projects/trackedTimeReport/interval/useSelectedInterval.ts
@@ -5,15 +5,26 @@ import {
 import { useStateCorrector } from '@lib/ui/state/useStateCorrector'
 import { Interval } from '@lib/utils/interval/Interval'
 import { useTotalIntervalLength } from './useTotalIntervalLength'
+import { useTimeGrouping } from '../timeGrouping/useTimeGrouping'
+import { TimeGrouping } from '../timeGrouping/TimeGrouping'
+
+const stateKey: Record<TimeGrouping, PersistentStateKey> = {
+  day: PersistentStateKey.TrackedTimeIntervalDays,
+  week: PersistentStateKey.TrackedTimeIntervalWeeks,
+  month: PersistentStateKey.TrackedTimeIntervalMonths,
+  year: PersistentStateKey.TrackedTimeIntervalYears,
+}
 
 export const useSelectedInterval = () => {
   const maxIntervalLength = useTotalIntervalLength()
 
+  const [group] = useTimeGrouping()
+
   return useStateCorrector(
-    usePersistentState<Interval>(
-      PersistentStateKey.TrackedTimeInterval,
-      () => ({ start: 0, end: maxIntervalLength - 1 }),
-    ),
+    usePersistentState<Interval>(stateKey[group], () => ({
+      start: 0,
+      end: maxIntervalLength - 1,
+    })),
     (value) => {
       if (value.end >= maxIntervalLength) {
         const end = maxIntervalLength - 1
