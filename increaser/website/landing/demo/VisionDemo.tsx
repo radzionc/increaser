df05ba4944a47fb3eaed696e997f21b99b85253f
Rodion Chachura <geekrodion@gmail.com>
2024-11-18 11:25:25 +0400

    Add VisionDemo component: implement vision display with ordered items and context providers

---
+++ increaser/website/landing/demo/VisionDemo.tsx
@@ -0,0 +1,23 @@
+import { useUser } from '@increaser/ui/user/state/user'
+import { order } from '@lib/utils/array/order'
+import { CurrentVisionAttributeProvider } from '@increaser/ui/vision/CurrentVisionAttributeProvider'
+import { ActiveItemIdProvider } from '@lib/ui/list/ActiveItemIdProvider'
+import { VisionBoardContainer } from '@increaser/ui/vision/VisionBoardContainer'
+import { VisionBoardItem } from '@increaser/ui/vision/VisionBoardItem'
+
+export const VisionDemo = () => {
+  const { vision } = useUser()
+  const items = order(Object.values(vision), (item) => item.order, 'asc')
+
+  return (
+    <ActiveItemIdProvider initialValue={null}>
+      <VisionBoardContainer>
+        {items.map((value) => (
+          <CurrentVisionAttributeProvider key={value.id} value={value}>
+            <VisionBoardItem />
+          </CurrentVisionAttributeProvider>
+        ))}
+      </VisionBoardContainer>
+    </ActiveItemIdProvider>
+  )
+}
--- increaser/website/landing/demo/VisionSlice.tsx
+++ increaser/website/landing/demo/VisionSlice.tsx
@@ -4,11 +4,11 @@ import {
 } from '@lib/ui/website/WebsiteSectionHeader'
 import { WebsiteSliceContent } from '@lib/ui/website/WebsiteSliceContent'
 import { getDemoSliceCopy } from './getDemoSliceCopy'
-import { MyVisionBoard } from '@increaser/ui/vision/MyVisionBoard'
 import { centeredContentColumn } from '@lib/ui/css/centeredContentColumn'
 import { WebsiteSlice } from '@lib/ui/website/WebsiteSlice'
 import styled from 'styled-components'
 import { DemoGuard } from '../../demo/DemoGuard'
+import { VisionDemo } from './VisionDemo'
 
 const Slice = styled(WebsiteSlice)`
   ${centeredContentColumn({
@@ -23,7 +23,7 @@ export const VisionSlice = (props: Partial<WebsiteSectionHeaderProps>) => {
       <WebsiteSliceContent>
         <WebsiteSectionHeader {...getDemoSliceCopy(id)} {...props} />
         <DemoGuard>
-          <MyVisionBoard />
+          <VisionDemo />
         </DemoGuard>
       </WebsiteSliceContent>
     </Slice>
