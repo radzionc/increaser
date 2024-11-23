5af19f47b2a38422f03b21374ebc2cc82b53c76e
Rodion Chachura <geekrodion@gmail.com>
2024-11-23 09:08:15 +0400

    Refactor getHabitStreak function: simplify date calculations by replacing convertDuration with differenceInDays; streamline logic for determining last day and passed days; enhance readability and maintainability.

--- increaser/entities-utils/habit/getHabitStreak.ts
+++ increaser/entities-utils/habit/getHabitStreak.ts
@@ -1,8 +1,7 @@
 import { toHabitDate } from '@increaser/entities-utils/habit/toHabitDate'
 import { ActiveHabit } from '@increaser/entities/Habit'
 import { range } from '@lib/utils/array/range'
-import { convertDuration } from '@lib/utils/time/convertDuration'
-import { endOfDay, subDays } from 'date-fns'
+import { endOfDay, subDays, differenceInDays } from 'date-fns'
 
 type GetHabitStreakInput = {
   at: number
@@ -17,27 +16,20 @@ export const getHabitStreak = ({
 
   const hasSuccessAtDate = successesSet.has(toHabitDate(at))
 
-  const passedDayEndedAt = hasSuccessAtDate
-    ? endOfDay(at).getTime()
-    : endOfDay(subDays(at, 1)).getTime()
+  const lastDayEndedAt = endOfDay(
+    hasSuccessAtDate ? at : subDays(at, 1),
+  ).getTime()
 
-  const daysCount = Math.max(
-    Math.ceil(convertDuration(passedDayEndedAt - startedAt, 'ms', 'd')),
-    0,
+  const daysCount = differenceInDays(lastDayEndedAt, startedAt)
+
+  const days = range(daysCount).map((index) =>
+    subDays(lastDayEndedAt, index).getTime(),
   )
 
-  const passedDays = range(daysCount)
-    .map((index) => passedDayEndedAt - convertDuration(index + 1, 'ms', 'd'))
-    .filter(
-      (timestamp) => timestamp > startedAt - convertDuration(1, 'd', 'ms'),
-    )
-
-  const index = passedDays.findIndex(
-    (day) => !successesSet.has(toHabitDate(day)),
-  )
+  const index = days.findIndex((day) => !successesSet.has(toHabitDate(day)))
 
   if (index === -1) {
-    return passedDays.length
+    return days.length
   }
 
   return index
