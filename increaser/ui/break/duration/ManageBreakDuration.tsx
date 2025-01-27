f46028366b0e3cfc5d159f813e138aa30522e982
Rodion Chachura <geekrodion@gmail.com>
2025-01-27 08:05:31 +0400

    Refactor ManageBreakDuration with usePresentState
    - Replace direct useBreakDuration with usePresentState
    - Remove unnecessary null check
    - Simplify state management for break duration selection

--- increaser/ui/break/duration/ManageBreakDuration.tsx
+++ increaser/ui/break/duration/ManageBreakDuration.tsx
@@ -1,11 +1,11 @@
 import { GroupedRadioInput } from '@lib/ui/inputs/GroupedRadioInput'
 import { useBreakDuration } from './state/useBreakDuration'
 import { breakDurations } from './BreakDuration'
+import { usePresentState } from '@lib/ui/state/usePresentState'
 
 export const ManageBreakDuration = () => {
-  const [value, setValue] = useBreakDuration()
+  const [value, setValue] = usePresentState(useBreakDuration())
 
-  if (!value) return null
   return (
     <GroupedRadioInput
       options={breakDurations.map((duration) => duration.toString())}
