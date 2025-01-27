addced2cf4cb8771dc03afc2b6d403808bdb50ff
Rodion Chachura <geekrodion@gmail.com>
2025-01-27 08:02:00 +0400

    Move FloatingBreakWidget rendering inside BreakManager

--- increaser/app/pages/_app.tsx
+++ increaser/app/pages/_app.tsx
@@ -19,8 +19,8 @@ import { UserManager } from '@increaser/ui/user/UserManager'
 import { UserStateOnly } from '@increaser/ui/user/UserStateOnly'
 import { FocusManager } from '@increaser/ui/focus/FocusManager'
 import { YouTubeFocusMusicProvider } from '@increaser/ui/focus/audio/youTube/YouTubeFocusMusicProvider'
-import { FloatingBreakWidget } from '@increaser/ui/break/widget/FloatingBreakWidget'
 import { BreakManager } from '@increaser/ui/break/BreakManager'
+
 const openSans = Open_Sans({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700', '800'],
@@ -51,7 +51,6 @@ function MyApp({ Component, pageProps }: MyAppProps) {
                     <UserStateOnly>
                       <FocusManager />
                       <BreakManager />
-                      <FloatingBreakWidget />
                     </UserStateOnly>
                   </AuthenticatedOnly>
                   {component}
--- increaser/ui/break/BreakManager.tsx
+++ increaser/ui/break/BreakManager.tsx
@@ -1,12 +1,18 @@
 import { AutoBreakManager } from './automation/AutoBreakManager'
 import { BreakAutoStop } from './BreakAutoStop'
 import { useBreakDuration } from './duration/state/useBreakDuration'
+import { FloatingBreakWidget } from './widget/FloatingBreakWidget'
 
 export const BreakManager = () => {
   const [breakDuration] = useBreakDuration()
 
   if (breakDuration) {
-    return <BreakAutoStop />
+    return (
+      <>
+        <BreakAutoStop />
+        <FloatingBreakWidget />
+      </>
+    )
   }
 
   return <AutoBreakManager />
