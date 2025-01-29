0b217950216746f79d235d024b95cfcc08500162
Rodion Chachura <geekrodion@gmail.com>
2025-01-29 08:34:45 +0400

    Change TextConnector from paragraph to span element

--- increaser/ui/preferences/TextConnector.tsx
+++ increaser/ui/preferences/TextConnector.tsx
@@ -1,7 +1,7 @@
 import { text } from '@lib/ui/text'
 import styled from 'styled-components'
 
-export const TextConnector = styled.p`
+export const TextConnector = styled.span`
   ${text({
     color: 'supporting',
     weight: 600,
